2a105385a59199ed563a74f42a7f45f4c1051bdd
Izzatbek <abdusharipovizzat03@gmail.com>
2026-07-17 16:17:16 +0500

    page structure added to ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Auth from "./pages/Auth"
+import ForgotPassword from "./pages/ForgotPassword"
+import ResetPassword from "./pages/ResetPassword"
+import VerifyOtp from "./pages/VerifyOtp"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/auth" element={<Auth/>} />
+      <Route path="/forgot-password" element={<ForgotPassword/>} />
+      <Route path="/reset-password" element={<ResetPassword/>} />
+      <Route path="/verify-otp" element={<VerifyOtp/>} />
+    </Routes>
+    </>
   )
 }
 
